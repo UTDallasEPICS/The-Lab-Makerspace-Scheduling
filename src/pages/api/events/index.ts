fb5035e49429b6a40944f67a8872b131fc9cbb10
Jude Joubert <joubertjude@gmail.com>
2024-10-16 15:51:42 -0500

    Remove prisma

--- src/pages/api/events/index.ts
+++ src/pages/api/events/index.ts
@@ -1,39 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '@/helpers/db';
+//import { prisma } from '@/helpers/db';
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (req.method === 'GET') {
-		const currentDate = new Date();
-		const events = await prisma.event.findMany({
-			where: { startTime: { gt: currentDate } },
-			orderBy: { startTime: 'asc' },
-		});
 
-		res.status(200).json(events);
-	}
-	// Route implementation will go here
-	if (req.method === 'POST') {
-		// Parse the request body to get the event data
-		const eventData = req.body;
-
-		// Validate the data and return an error response if necessary
-		const modifiedEventData = { ...eventData };
-		modifiedEventData.startTime = new Date(eventData.startTime);
-		modifiedEventData.endTime = new Date(eventData.endTime);
-
-		// Use Prisma to create the event in the database
-		const newEvent = await prisma.event.create({
-			data: eventData,
-		});
-
-		// Return a success response with the created event data
-		res.status(201).json(newEvent);
-	} else {
-		// Return a 405 response for unsupported request methods
-		res.status(405).json({ message: 'Method not allowed' });
-	}
-	res.status(405).json({ message: 'Method not allowed' });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,40 @@
 import Layout from '../components/layout';
 import Calendar from '../components/Calendar';
-import { prisma } from '@/helpers/db';
 import { GetServerSideProps } from 'next';
 import { EventInput } from '@fullcalendar/core';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	const events = await prisma.event.findMany({});
+  //const api_url = process.env.API_URL;
+  const api_url = 'http://localhost:3030';
+  const events = await fetch(`${api_url}/api/events`).then((response) => { return response.json() });
+
 	const conformedEvents = events.map((calEvent) => {
 		const fullCalEvent: EventInput = {
-			title: calEvent.title,
-			start: calEvent.startTime.toISOString(),
-			end: calEvent.endTime.toISOString(),
+			title: calEvent.name,
+			//start: new Date(calEvent.start).toISOString(),
+	     start: calEvent.start,
+			//end: new Date(calEvent.end).toISOString(),
+	     end: calEvent.end,
 			url: `/events/${calEvent.id}`,
 		};
-		return fullCalEvent;
+
+	   return fullCalEvent;
 	});
-	return {
+
+  return {
 		props: {
 			events: conformedEvents,
 		},
 	};
 };
 
-export default function IndexPage({ events }: { events: EventInput[] }) {
+export default function IndexPage({ }: { events: EventInput[] }) {
+  const events = getServerSideProps();
+
 	return (
 		<Layout headerText='Calendar View'>
-			<div style={{ width: '100%' }}>
-				<Calendar events={events} />
+	     <div style={{ width: '100%' }}>
+			  <Calendar events={events} />
 			</div>
 		</Layout>
 	);
